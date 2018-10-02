c1b66876f65f9f160eb8960a09d4785654938a1b
tanaka_taishiro <>
2018-10-02 21:24:38 +0900

    fix: TodoのValueオブジェクト名の修正

--- model/tweet.py
+++ model/tweet.py
@@ -11,17 +11,17 @@ class TweetId():
         return type(self.value) is int
 
 
-class TweetContent():
-    def __init__(self, tweet_content: str) -> None:
-        self.value = tweet_content
+class Content():
+    def __init__(self, content: str) -> None:
+        self.value = content
 
     def validate_type(self) -> bool:
         return type(self.value) is str
 
 
-class TweetCreateDate():
-    def __init__(self, tweet_create_date: str) -> None:
-        self.value = tweet_create_date
+class CreateDate():
+    def __init__(self, create_date: str) -> None:
+        self.value = create_date
 
     def validate_type(self) -> bool:
         return type(self.value) is str
@@ -30,9 +30,9 @@ class TweetCreateDate():
         return len(self.value) <= 140
 
 
-class TweetDeleteStatus():
-    def __init__(self, tweet_delete_status: int) -> None:
-        self.value = tweet_delete_status
+class DeleteStatus():
+    def __init__(self, delete_status: int) -> None:
+        self.value = delete_status
 
     def validate_type(self) -> bool:
         if self.value == int(0):
@@ -46,13 +46,13 @@ class TweetDeleteStatus():
 # エンティティ
 
 class Tweet():
-    def __init__(self, tweet_id: TweetId, tweet_content: TweetContent,
-                 tweet_create_date: TweetCreateDate,
-                 tweet_delete_status: TweetDeleteStatus) -> None:
+    def __init__(self, tweet_id: TweetId, content: Content,
+                 create_date: CreateDate,
+                 delete_status: DeleteStatus) -> None:
         self.tweet_id = tweet_id
-        self.tweet_content = tweet_content
-        self.tweet_create_date = tweet_create_date
-        self.tweet_delete_status = tweet_delete_status
+        self.tweet_content = content
+        self.tweet_create_date = create_date
+        self.tweet_delete_status = delete_status
 
     def to_dict(self):
         return {
